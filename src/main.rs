703ea98d1630aee8f8f33d8293042edbe5bf18f5
Dustin Cannon <dustin.cannon@gmail.com>
2023-10-26 10:42:44 -0700

    Basic arg parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of environment / habitat to use. E.g. int/stg/prd
+    #[arg(short, long)]
+    env: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("habitat is {}", args.env);
 }
