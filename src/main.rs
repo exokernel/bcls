da5f85914219d4bb6045f771007e78fa0431b8ac
Dustin Cannon <dustin.cannon@gmail.com>
2023-10-26 10:56:19 -0700

    Add pattern argument

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ struct Args {
     /// Name of environment / habitat to use. E.g. int/stg/prd
     #[arg(short, long)]
     env: String,
+
+    /// Search pattern for instance names. E.g. "^store-lb"
+    pattern: String,
 }
 
 fn main() {
     let args = Args::parse();
 
     println!("habitat is {}", args.env);
+    println!("pattern is {}", args.pattern);
 }
