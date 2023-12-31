308c112a526fedaa094d3678c8281f3c6a80ca36
Dustin Cannon <dustin.cannon@gmail.com>
2023-12-30 23:07:26 -0800

    test Instance::from_json()

--- src/compute/records.rs
+++ src/compute/records.rs
@@ -77,3 +77,37 @@ impl Instance {
         )
     }
 }
+
+// Test section
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_instance_from_json() {
+        let json = json!({
+            "name": "test-instance",
+            "networkInterfaces": [
+                {
+                    "networkIP": "127.0.0.1"
+                }
+            ],
+            "zone": "test-zone",
+            "machineType": "test-machine-type",
+            "cpuPlatform": "test-cpu-platform",
+            "status": "test-status",
+            "labels": "test-labels"
+        });
+
+        let instance = Instance::from_json(json).unwrap();
+        assert_eq!(instance.name, "test-instance");
+        assert_eq!(instance.ip, "127.0.0.1");
+        assert_eq!(instance.zone, "test-zone");
+        assert_eq!(instance.machine_type, "test-machine-type");
+        assert_eq!(instance.cpu_platform, "test-cpu-platform");
+        assert_eq!(instance.status, "test-status");
+        assert_eq!(instance.labels, "test-labels");
+    }
+}
