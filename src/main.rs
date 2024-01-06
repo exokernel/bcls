626ebe7f4cdec2a544be7e3a704a6099c6db9a0b
Dustin Cannon <dustin.cannon@gmail.com>
2024-01-06 01:47:06 -0800

    use to_owned instead of to_string

--- src/main.rs
+++ src/main.rs
@@ -84,7 +84,7 @@ fn show_instances(
     _long: bool,
     _ip: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let c = bcls::compute::Compute::new(project.to_string(), bcls::http::Http::default());
+    let c = bcls::compute::Compute::new(project.to_owned(), bcls::http::Http::default());
     let instances = c.list_instances(pattern);
     match instances {
         Ok(instances) => {
