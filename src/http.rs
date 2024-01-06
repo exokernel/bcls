01605320f183333db74ced1d03f5294bc3af6e0f
Dustin Cannon <dustin.cannon@gmail.com>
2024-01-06 01:01:40 -0800

    more idiomatic and simplified module structure

--- src/http.rs
+++ src/http.rs
@@ -1,6 +1,43 @@
-mod http;
+// This will be a wrapper around reqwest::blocking::Client that provides a Trait we can use to for mocking in tests
 
-pub use self::http::{Http, HttpTrait};
+use reqwest::blocking::Client as ReqwestClient;
+use serde_json::Value as JsonValue;
 
-#[cfg(test)]
-pub use self::http::MockHttpTrait;
+// Trait for our http app service
+#[cfg_attr(test, mockall::automock)]
+pub trait HttpTrait {
+    fn get(&self, token: &str, url: &str) -> Result<JsonValue, Box<dyn std::error::Error>>;
+}
+
+// A struct for our compute app service that implements the HttpTrait
+pub struct Http {
+    client: ReqwestClient,
+}
+
+impl Default for Http {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Http {
+    // A builder function for our compute app service
+    pub fn new() -> Self {
+        Http {
+            client: ReqwestClient::new(),
+        }
+    }
+}
+
+// Implement the HttpTrait for our Http struct
+impl HttpTrait for Http {
+    fn get(&self, token: &str, url: &str) -> Result<JsonValue, Box<dyn std::error::Error>> {
+        let resp = self
+            .client
+            .get(url)
+            .bearer_auth(token.to_owned())
+            .send()?
+            .json::<JsonValue>()?;
+        Ok(resp)
+    }
+}
